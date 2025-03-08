822cfa520492cb2aaa471d170c3d048567475650
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-03-08 15:54:19 -0400

    Agregada ruta contar registros por coleccion

--- src/index.js
+++ src/index.js
@@ -19,6 +19,17 @@ app.use("/productos", productosRouter);
 app.get("/op", (req, res) => {
   res.json({ totalOperaciones: req.app.locals.total || 0 });
 });
+app.get("/contar", async (req, res) => {
+  try {
+    const Usuario = require("./models/Usuario");
+    const Producto = requier("./models/Producto");
+    const usuariosCount = await Usuario.countDocuments();
+    const productosCount = await Producto.countDocuments();
+    res.json({ usuariosCount, productosCount });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
